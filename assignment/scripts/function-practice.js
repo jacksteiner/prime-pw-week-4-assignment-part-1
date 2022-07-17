53a6b7917fa67e6ac7b6df4a5ebb0bd2638998db
Jack Steiner <steiner.jack01@gmail.com>
2022-07-17 14:53:33 -0500

    finished number 7, just needed to add a for loop

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -16,11 +16,11 @@ hello ();
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  console.log ( 'Hello Jimbo' );
+  console.log ( 'Hello' + name );
   return;
 }
 // Remember to call the function to test
-helloName ();
+helloName (' Jack');
 
 
 // 3. Function to add two numbers together & return the result
@@ -73,38 +73,44 @@ console.log(getLast([]));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
+
+numberArray= [1,2,3,4,5];
 function find( value, array ){
   let check = false;
+
+for (let i = 0; i < numberArray.length; i++ ){
   //todo forloop
+
 if (value === [i]) {
      check = true; }
      return check
 }
-
+}
+find(3 , numberArray)
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
-//    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
+// // 8. Function to check if a letter is the first letter in a 
+// //    string. Return true if it is, and false otherwise
+// function isFirstLetter(letter, string) {
 
-}
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+// }
+// console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+// console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
-// 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
-  return sum;
-}
+// // 9. Function to return the sum of all numbers in an array
+// function sumAll( ) {
+//   let sum = 0
+//   // TODO: loop to add items
+//   return sum;
+// }
 
-// 10. Function to return a new array of all positive (greater than zero)
-//     numbers contained in an input array. If there are no positive numbers
-//     return an empty array. Note: The input array should not change.
+// // 10. Function to return a new array of all positive (greater than zero)
+// //     numbers contained in an input array. If there are no positive numbers
+// //     return an empty array. Note: The input array should not change.
 
 
 
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
-//     here in a comment, write the function, and test it!
+// // 11. Pick a problem from Edabit(https://edabit.com/) or 
+// //     CodeWars(https://www.codewars.com/). Then describe it 
+// //     here in a comment, write the function, and test it
